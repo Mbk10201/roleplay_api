5b9e570c1e66870c60ca1974aef6340a69ac1e30
MBK <74474524+Mbk10201@users.noreply.github.com>
2023-08-27 14:41:55 +0200

    [Job - Hospital] Added grades

---
+++ code/Jobs/List/JobHospital.cs
@@ -0,0 +1,42 @@
+﻿using Mbk.RoleplayAPI.World;
+
+namespace Mbk.RoleplayAPI.Jobs.List;
+
+internal class JobHospital : Job
+{
+	public override string Identifier => "hospital";
+
+	public JobHospital()
+	{
+		Name = "Hospital";
+		CanSell = true;
+
+		Grades.Add( new Grade()
+		{
+			Id = 1,
+			Name = "Chef",
+			Salary = 2500
+		} );
+
+		Grades.Add( new Grade()
+		{
+			Id = 2,
+			Name = "Superviseur",
+			Salary = 2500
+		} );
+
+		Grades.Add( new Grade()
+		{
+			Id = 3,
+			Name = "Coordinateur",
+			Salary = 2500
+		} );
+
+		Grades.Add( new Grade()
+		{
+			Id = 4,
+			Name = "Paramedic",
+			Salary = 2500
+		} );
+	}
+}

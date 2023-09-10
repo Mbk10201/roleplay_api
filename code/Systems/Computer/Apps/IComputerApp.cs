73c76f4ebac53ce3b5af36ac2cd432fb34066887
MBK <74474524+Mbk10201@users.noreply.github.com>
2023-09-10 12:27:05 +0200

    Added App job access & roles access + Implemented police app

--- code/Systems/Computer/Apps/IComputerApp.cs
+++ code/Systems/Computer/Apps/IComputerApp.cs
@@ -6,6 +6,23 @@ public interface IComputerApp : IWindow
 {
 	/// <summary>
 	/// The name of this app.
+	/// (Required)
 	/// </summary>
 	string AppName { get; }
+
+	/// <summary>
+	/// The jobs that has access to this app.
+	/// /// Use like : new string[]{"police", "hospital"}
+	/// (Optional)
+	/// If optional leave default value as ""
+	/// </summary>
+	string[] AppJobAccess { get; }
+
+	/// <summary>
+	/// The roles id that has access to this app.
+	/// Use like : new long[]{40000}
+	/// (Optional)
+	/// If optional leave default value as -1
+	/// </summary>
+	long[] AppRoleId { get; }
 }
